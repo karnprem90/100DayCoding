c91eb28b4673c49be8690e49230e155ab9ee2c42
Prem Bhushan Karn <karnprem90@gmail.com>
2025-08-09 10:30:33 +0530

    Code

---
+++ Leetcode/143.js
@@ -0,0 +1,86 @@
+/**
+ * You are given the head of a singly linked-list. The list can be represented as:
+ *
+ * L0 → L1 → … → Ln - 1 → Ln
+ * Reorder the list to be on the following form:
+ *
+ * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+ * You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4]
+ * Output: [1,4,2,3]
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [1,5,2,4,3]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is in the range [1, 5 * 104].
+ * 1 <= Node.val <= 1000
+ */
+
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    if (!head || !head.next) {
+        return null;
+    }
+
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let second = reverseLinkedList(slow.next);
+    slow.next = null;
+
+
+    let first = head;
+    while (second) {
+        const t1 = first.next;
+        const t2 = second.next;
+        first.next = second;
+        second.next = t1;
+        first = t1;
+        second = t2;
+    }
+    return head;
+
+};
+
+function reverseLinkedList(ll) {
+    let prev = null;
+    let current = ll;
+    let next = null;
+    while (current) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+
+console.log(reorderList(head));
---
+++ Leetcode/2130.js
@@ -0,0 +1,92 @@
+/**
+ * In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+ *
+ * For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
+ * The twin sum is defined as the sum of a node and its twin.
+ *
+ * Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [5,4,2,1]
+ * Output: 6
+ * Explanation:
+ * Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+ * There are no other nodes with twins in the linked list.
+ * Thus, the maximum twin sum of the linked list is 6.
+ * Example 2:
+ *
+ *
+ * Input: head = [4,2,2,3]
+ * Output: 7
+ * Explanation:
+ * The nodes with twins present in this linked list are:
+ * - Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
+ * - Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
+ * Thus, the maximum twin sum of the linked list is max(7, 4) = 7.
+ * Example 3:
+ *
+ *
+ * Input: head = [1,100000]
+ * Output: 100001
+ * Explanation:
+ * There is only one node with a twin in the linked list having twin sum of 1 + 100000 = 100001.
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is an even integer in the range [2, 105].
+ * 1 <= Node.val <= 105
+ */
+
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let reversedLL = reverseLinkedList(slow);
+    let maxSum = -Infinity;
+    while (head && reversedLL) {
+        let val1 = head.val;
+        let val2 = reversedLL.val;
+        maxSum = Math.max(maxSum, (val1 + val2));
+        head = head.next;
+        reversedLL = reversedLL.next;
+    }
+    console.log(maxSum);
+};
+
+function reverseLinkedList(ll) {
+    let prev = null;
+    let current = ll;
+    let next = null;
+    while (current) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+
+let head = new ListNode(4);
+head.next = new ListNode(2);
+head.next.next = new ListNode(2);
+head.next.next.next = new ListNode(3);
+
+console.log(pairSum(head));
